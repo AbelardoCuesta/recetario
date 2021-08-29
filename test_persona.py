878e374a08657c4fe03c24d76b259a551f0f35ef
AbelardoCuesta <j.cuestaa@uniandes.edu.co>
2021-08-29 12:30:07 -0500

    Update test_persona.py

--- test_persona.py
+++ test_persona.py
@@ -11,7 +11,7 @@ class PersonaTestCase(unittest.TestCase):
       self.persona3 = Persona(nombre='Alejandra', edad=25)
       self.persona4 = Persona(nombre='Diana', edad=25)
       self.grupo = [self.persona1, self.persona2, self.persona3]
-
+      #aquiestoy
    def test_constructor(self):
       self.assertEqual(self.persona1.dar_nombre(), 'Alejandra')
       self.assertEqual(self.persona1.dar_edad(), 30)
